4b71aa2a55674a2380605554ec53b7846ea5aa23
Jesper Haglöf <jesper@playmaker.ai>
2022-08-09 20:39:14 +0200

    Added step to get roster end links from espn nfl page: http://www.espn.com/nfl/players

--- scrapers/scrape_espn.py
+++ scrapers/scrape_espn.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 class espnScraper:
     def __init__(self, league_link):
+        self.roster_end_links = None
         self.link = league_link
         self.urls = []
         self.df = pd.DataFrame(columns=['Team', 'Pos', 'Player', 'Age', 'Nationality', 'Market Value (mEuros)'])
@@ -17,14 +18,34 @@ class espnScraper:
         soup = BeautifulSoup(html_text, 'html.parser')
         return soup
 
-    def scrape(self):
+    def _scrape_roster_end_links(self):
 
         # Scrape ESPN
+        team_page_soup = self.get_soup(self.link)
+        # Get all tables from team page
+        tables = team_page_soup.find_all('div', class_="mod-container mod-open-list mod-no-footer mod-teams-list-small")
+        # loop through to access all roster links
+        for line in tables:
+            print(line)
+            elements = line.find_all('a')
+            for element in elements:
+                if "padding-top" in str(element):
+                    roster_link = str(element).split('href="')[1].split('" ')[0]
+                    print(roster_link)
+                    self.roster_end_links.append(roster_link)
+
+    def scrape(self):
+
+        # Step 1: Get roster end links from team page
+        self._scrape_roster_end_links()
+
+
+
 
 
 if __name__ == '__main__':
 
-    url = 'https://www.espn.com/nfl/team/roster/_/name/buf/buffalo-bills'
+    url = 'http://www.espn.com/nfl/players'
     Scraper = espnScraper(url)
     Scraper.scrape()
 
